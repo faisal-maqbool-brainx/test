187361360bebf3b18187064f904cc65bb55c0134
Faisal Maqbool <faisal.maqbool@brainxtech.com>
2019-10-07 19:17:25 +0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-#Default is "World"
-#author Faisal Maqbool (faisal.maqbool@brainxtech.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
